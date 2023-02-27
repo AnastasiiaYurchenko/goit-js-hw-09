d6a8b7aef95fbc1f074a2d1e89a3f93a992a5729
AnastasiiaYurchenko <113210617+AnastasiiaYurchenko@users.noreply.github.com>
2023-02-27 22:40:06 +0000

    Deploying to gh-pages from @ AnastasiiaYurchenko/goit-js-hw-09@03b3561851e209bb9878825177ea865796e32585 🚀

---
+++ 01-color-switcher.f55b5436.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(e=>{t.setAttribute("disabled",!0),intervalId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(e=>{t.removeAttribute("disabled"),clearInterval(intervalId)}));
+//# sourceMappingURL=01-color-switcher.f55b5436.js.map

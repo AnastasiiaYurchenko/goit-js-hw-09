7646cfdd0a111fe4b22ce52e4091f4ddd9453259
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-02-28 23:09:29 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,13 @@
 
+// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
 const startBtn = document.querySelector("button[data-start]");
 const stopBtn = document.querySelector("button[data-stop]");
 const bodyEl = document.querySelector("body");
+
+stopBtn.setAttribute("disabled", true); 
+let intervalId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -10,6 +16,9 @@ function getRandomHexColor() {
 const onStartBtnClick = (e) => {
     // console.log("onStartBtnClick");
     startBtn.setAttribute("disabled", true);
+    if (stopBtn.hasAttribute("disabled")) {
+     stopBtn.removeAttribute("disabled");   
+    };
     intervalId = setInterval(() => {
         // console.log("Працює інтервал");
     bodyEl.style.backgroundColor = getRandomHexColor();
@@ -17,8 +26,11 @@ const onStartBtnClick = (e) => {
 };
 
 const onStopBtnClick = (e) => {
+    if (startBtn.hasAttribute("disabled")) {
     startBtn.removeAttribute("disabled");
+    };
     // console.log("onStopBtnClick");
+    stopBtn.setAttribute("disabled", true);
     clearInterval(intervalId); 
 };
 

e1f32767ab67400456c8a91167591c09669c0f2f
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-03-02 00:50:29 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -26,6 +26,29 @@ labelsEl.forEach(label => {
     label.style.textTransform = "uppercase";
 });
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -33,6 +56,12 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    const countDownDate = selectedDates[0].getTime();
+    console.log(countDownDate);
+    const nowDate = Date.now();
+    console.log(nowDate);
+    const deltaDates = countDownDate - nowDate;
+console.log(deltaDates);
   },
 };
 
@@ -42,4 +71,12 @@ const onStartBtnClick = () => {
     console.log("onStartBtnClick");
 };
 
+// const countDownDate = options.defaultDate.getTime();
+// console.log(countDownDate);
+// const nowDate = Date.now();
+// console.log(nowDate);
+// const deltaDates = countDownDate - nowDate;
+// console.log(deltaDates);
+
 startBtn.addEventListener("click", onStartBtnClick);
+

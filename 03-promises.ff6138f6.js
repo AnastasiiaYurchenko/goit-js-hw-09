56716b7ef197536a24d65177547194c637a82c37
AnastasiiaYurchenko <113210617+AnastasiiaYurchenko@users.noreply.github.com>
2023-03-04 23:05:02 +0000

    Deploying to gh-pages from @ AnastasiiaYurchenko/goit-js-hw-09@a51acfccc81c8641a845d90cd55876b4b0745004 🚀

---
+++ 03-promises.ff6138f6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form"),t=(document.querySelector('input[name="delay"]'),document.querySelector('input[name="step"]'),document.querySelector('input[name="amount"]'),document.querySelector("button"));console.log(t);e.addEventListener("submit",(e=>{e.preventDefault()})),t.addEventListener("submit",(()=>{}));
+//# sourceMappingURL=03-promises.ff6138f6.js.map

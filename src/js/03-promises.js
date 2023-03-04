a51acfccc81c8641a845d90cd55876b4b0745004
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-03-05 01:04:22 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,18 @@
+const formEl = document.querySelector('.form');
+const inputFirstDelay = document.querySelector('input[name="delay"]');
+const inputDelayStep = document.querySelector('input[name="step"]');
+const inputAmount = document.querySelector('input[name="amount"]');
+const buttonEl = document.querySelector('button');
+console.log(buttonEl); 
+
+const onFormElSubmit = (event) => {
+  event.preventDefault();
+};
+
+const onButtonElSubmit = () => {
+
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +21,6 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+formEl.addEventListener("submit", onFormElSubmit);
+buttonEl.addEventListener("submit", onButtonElSubmit);

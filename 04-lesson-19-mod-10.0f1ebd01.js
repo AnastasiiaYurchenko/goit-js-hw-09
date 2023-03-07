37c154ef9d6323b2e11dd32507e5c7d03d74204d
AnastasiiaYurchenko <113210617+AnastasiiaYurchenko@users.noreply.github.com>
2023-03-07 20:09:44 +0000

    Deploying to gh-pages from @ AnastasiiaYurchenko/goit-js-hw-09@b2c9730dc4965b61ac61e5b08b053462bfa4e966 🚀

--- 04-lesson-19-mod-10.0f1ebd01.js
+++
@@ -1,2 +0,0 @@
-fetch("https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=9d46ea07e04d4f7f9708dcaed27cd554").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log("error",e)}));
-//# sourceMappingURL=04-lesson-19-mod-10.0f1ebd01.js.map
